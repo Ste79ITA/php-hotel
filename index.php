af69a46b896f445ea76471546c4b09ad5990c542
Ste79ITA <nastri.s@gmail.com>
2023-11-22 16:38:22 +0100

    Milestone 2

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
-<body>
-    
-</body>
-</html>
 
 <?php
 
@@ -52,8 +51,103 @@
 
     ];
 
-    foreach ($hotels as $key => $value) {
-        var_dump($value);
-
-    }
+    
 ?> 
+
+<body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Hotel</th>
+      <th scope="col">1</th>
+      <th scope="col">2</th>
+      <th scope="col">3</th>
+      <th scope="col">4</th>
+      <th scope="col">5</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Name</th>
+<?php 
+foreach ($hotels as $hotel) {
+    foreach ($hotel as $key => $item ) {
+        if ($key == 'name') {
+            ?>
+           <td><?php echo $item ?></td>
+            <?php
+        }
+    }
+
+}
+
+?>
+      
+    </tr>
+    <tr>
+      <th scope="row">Description</th>
+      <?php 
+foreach ($hotels as $hotel) {
+    foreach ($hotel as $key => $item ) {
+        if ($key == 'description') {
+            ?>
+           <td><?php echo $item ?></td>
+            <?php
+        }
+    }
+
+}
+
+?>
+    </tr>
+    <tr>
+      <th scope="row">Parking</th>
+      <?php 
+foreach ($hotels as $hotel) {
+    foreach ($hotel as $key => $item ) {
+        if ($key == 'parking') {
+            ?>
+           <td><?php echo $item ? 'Yes' : 'No' ?></td>
+            <?php
+        }
+    }
+
+}
+
+?>
+    </tr>
+    <tr>
+      <th scope="row">Vote</th>
+      <?php 
+foreach ($hotels as $hotel) {
+    foreach ($hotel as $key => $item ) {
+        if ($key == 'vote') {
+            ?>
+           <td><?php echo $item ?></td>
+            <?php
+        }
+    }
+
+}
+?>
+    </tr>
+    <tr>
+      <th scope="row">Distance to Center</th>
+      <?php 
+foreach ($hotels as $hotel) {
+    foreach ($hotel as $key => $item ) {
+        if ($key == 'distance_to_center') {
+            ?>
+           <td><?php echo $item ?>Km</td>
+            <?php
+        }
+    }
+
+}
+?>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
+
